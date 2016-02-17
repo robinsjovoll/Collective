7eb339ad3a2dd6d212c0757b4c2424119e371b51
Kenneth <kennethwestli@gmail.com>
2016-02-17 14:25:50 +0100

    Household class

---
+++ app/src/main/java/com/mobile/collective/Household.java
@@ -0,0 +1,17 @@
+package com.mobile.collective;
+
+import java.util.ArrayList;
+
+public class Household {
+    private ArrayList<User> members, admins;
+    private final int roomKey;
+    //private Scoreboard scoreboard;
+    private ArrayList<Task> tasks;
+
+    public Household(){
+        this.roomKey = 0;
+    }
+
+
+
+}
