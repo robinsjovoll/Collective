2ba8489fb528ce731ba93016ce3ddceab46f3ad2
Kenneth <kennethwestli@gmail.com>
2016-02-24 21:42:11 +0100

    Updated model classes

--- app/src/main/java/com/mobile/collective/implementation/model/Flat.java
+++ app/src/main/java/com/mobile/collective/implementation/model/Flat.java
@@ -4,39 +4,114 @@ import com.mobile.collective.implementation.model.Task;
 import com.mobile.collective.implementation.model.User;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Flat {
     private ArrayList<User> flatmates;
     private final int flatPin;
     private ArrayList<Task> tasks;
+    private Period period;
+    private String prize;
+    private User admin; //In case of only one admin per flat.
 
-    public Flat(){
+
+    //suggest increase and decrease period in the create flat-view: <|-| BIWEEKLY |+|>
+    private enum Period{
+        WEEKLY, BIWEEKLY, MONTHLY {
+            //return current if no next.
+            public Period next(){
+                return this;
+            }
+            //return current if no previous.
+            public Period previous(){
+                return this;
+            }
+        };
+
+        public Period next(){
+            return values()[ordinal() + 1];
+        }
+
+        public Period previous(){
+            return values()[ordinal() - 1];
+        }
+    }
+
+    public Flat(User user, Period period, String prize){
+        this.period = period;
+        this.prize = prize;
         this.flatPin = generatePin();
         this.flatmates = new ArrayList<>();
         this.tasks = new ArrayList<>();
         addDefaultTasks();
+
+        this.flatmates.add(user);
+        user.makeAdmin();
+        this.admin = user;
     }
 
-    private int generatePin(){
-        return 0;
+    public String getPrize(){
+        return this.prize;
+    }
+
+    public void setPrize(String prize){
+        this.prize = prize;
+    }
+
+    public void increasePeriod(){
+        this.period.next();
+    }
+
+    public void decreasePeriod(){
+        this.period.previous();
+    }
+
+    public Period getPeriod(){
+        return this.period;
     }
 
     public boolean addFlatmate(User user){
-        return flatmates.add(user);
+        return this.flatmates.add(user);
     }
 
     public boolean removeFlatmate(User user){
-        return flatmates.remove(user);
+        return this.flatmates.remove(user);
+    }
+
+    public boolean promoteFlatmate(User user){
+        if(flatmates.contains(user)){
+            user.makeAdmin();
+            this.admin = user;
+            return true;
+        }else{
+            return false;
+        }
     }
 
     public ArrayList<User> getFlatmates(){
-        return flatmates;
+        return this.flatmates;
+    }
+
+    public boolean addTask(Task task){
+        return this.tasks.add(task);
+    }
+
+    //Should admin have to approve a completed task?
+    public void completeTask(User user, Task task, Date date){
+        user.increaseScore(task.getPoints());
+        task.updateHistory(date, user);
+        if (!task.getContinuous()){
+            tasks.remove(task);
+        }
+    }
+
+    private int generatePin(){
+        //TODO
+        return 0;
     }
 
     private void addDefaultTasks(){
         //TODO
     }
-
-
 
 }
